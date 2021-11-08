de89a2d1e30c70848040e89293d3dd34cc57d9d3
Marcus von Wrede <m.vonwrede@gmx.de>
2021-11-08 19:44:53 +0100

    stupid coach challenge

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,8 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = ['Great!', 'Silly question, get dressed and go to work!', 'I don`t care, get dressed and go to work!']
+  end
+end
